3a916268f17712aa339e33e4c6be8b1e6ee46c5e
drewvex23 <drewveilleux24@gmail.com>
2023-10-31 12:29:36 -0400

    commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const rockButton = document.getElementById('rock')
+const paperButton = document.getElementById('paper')
+const scissorsButton = document.getElementById('scissors')
+const results = document.querySelector('.result') 
+const compImg = document.querySelector('.comp-img') 
+const userImg = document.querySelector('.user-img') 
