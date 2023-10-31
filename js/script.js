7957134958967df8dec824ec2df4b2b0d28fecd6
drewvex23 <drewveilleux24@gmail.com>
2023-10-31 12:43:44 -0400

    commit

--- js/script.js
+++ js/script.js
@@ -4,3 +4,23 @@ const scissorsButton = document.getElementById('scissors')
 const results = document.querySelector('.result') 
 const compImg = document.querySelector('.comp-img') 
 const userImg = document.querySelector('.user-img') 
+
+
+
+function rockBtnClick() {
+    results.innerText = 'rock clicked'
+}
+
+function paperBtnClick() {
+    results.innerText = 'paper clicked'
+}
+
+function scissorsBtnClick() {
+    results.innerText = 'scissors clicked'
+}
+
+
+rockButton.addEventListener('click', rockBtnClick)
+paperButton.addEventListener('click', paperBtnClick)
+scissorsButton.addEventListener('click', scissorsBtnClick)
+
