8dc06ad5eb012942cf37b170da02e19df373cfe2
drewvex23 <drewveilleux24@gmail.com>
2023-11-02 12:44:44 -0400

    commit

--- js/script.js
+++ js/script.js
@@ -5,22 +5,34 @@ const results = document.querySelector('.result')
 const compImg = document.querySelector('.comp-img') 
 const userImg = document.querySelector('.user-img') 
 
+const choices = ['rock','paper','scissors']
+let comp
 
+rockButton.addEventListener('click', () => {
+    userImg.src = "../Images/Rps-images/" + rockButton.id + ".png"
+    compChoice()
+    winLose(rockButton.id)
+})
+paperButton.addEventListener('click', () => {
+    userImg.src = "../Images/Rps-images/" + paperButton.id + ".png"
+    compChoice()
+    winLose(paperButton.id)
+})
+scissorsButton.addEventListener('click', () => {
+    userImg.src = "../Images/Rps-images/" + scissorsButton.id + ".png"
+    compChoice()
+    winLose(scissorsButton.id)
+})
 
-function rockBtnClick() {
-    results.innerText = 'rock clicked'
+function compChoice() {
+    const randNum = Math.floor(Math.random()*3)
+    comp = choices[randNum]
+    compImg.src = "../Images/Rps-images/" + comp + ".png"
 }
 
-function paperBtnClick() {
-    results.innerText = 'paper clicked'
+function winLose(userChoice) {
+    if (comp == userChoice) {
+        results.innerText = 'Tie'
+    }
+        
 }
-
-function scissorsBtnClick() {
-    results.innerText = 'scissors clicked'
-}
-
-
-rockButton.addEventListener('click', rockBtnClick)
-paperButton.addEventListener('click', paperBtnClick)
-scissorsButton.addEventListener('click', scissorsBtnClick)
-
