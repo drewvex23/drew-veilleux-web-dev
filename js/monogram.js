d6b66427d1c56a3cfc5a5c97594009c49a92f173
drewvex23 <drewveilleux24@gmail.com>
2024-01-04 12:45:11 -0500

    commit

---
+++ js/monogram.js
@@ -0,0 +1,21 @@
+const productList = document.querySelectorAll('.product')
+const priceList = document.querySelectorAll('.item-price')
+const shopBtnList = document.querySelectorAll('.shop-btn')
+
+
+productList.forEach(product => {
+    product.addEventListener('mouseover', () => {
+        let index = product.id
+        shopBtnList[index].style.display = 'block' 
+        priceList[index].style.display = 'none'
+    })
+})
+
+productList.forEach(product => {
+    product.addEventListener('mouseout', () => {
+        let index = product.id
+        shopBtnList[index].style.display = 'none' 
+        priceList[index].style.display = 'block'
+    })
+})
+
